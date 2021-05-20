ea34fd05ef2b8303342b2b67d6381295e13af902
fotios <fotios@fundalytica.com>
2021-05-20 04:05:29 +0000

    annualized premium, send mail functionality

--- futures-socket.py
+++ futures-socket.py
@@ -5,6 +5,8 @@
 
 from futures import Futures
 
+# from mailer import Mailer
+
 import websocket
 import _thread
 import time
@@ -12,29 +14,62 @@ import sys
 import json
 
 from colorama import Fore
+from datetime import datetime, timezone, timedelta
 
-from utils import utils
+from utils import utils, mailer
 
 ws = None
 
+futures = None
+email_sent = False
+
+def send_email():
+    try:
+        global email_sent
+        if not email_sent:
+            recipients = ['fotios@fundalytica.com']
+            subject = 'Hello'
+            text = 'This is Mailgun!'
+            response = mailer.send_simple_message(recipients, subject, text)
+            email_sent = True
+            print(response)
+            print(response.text)
+            utils.cprint('Email Sent!', Fore.GREEN)
+    except Exception as e:
+        print(e)
+
 def ws_message(ws, message):
     # utils.cprint("\nWebSocket Thread: %s" % message, Fore.YELLOW)
-
     # print(message)
 
     m = json.loads(message)
     # print(m)
 
+    crypto_colors = {'XBT':Fore.GREEN, 'ETH':Fore.CYAN}
+
     if m['product_id']:
         if m['tag'] == 'semiannual':
-            print(f"{m['product_id']} {m['tag']} {m['markPrice']} {m['premium']}")
+            # XBT:USD FI_XBTUSD_210924 semiannual 38580.25 0.5% in 127d 1.4%
 
-    # SEND THE EMAIL !!!!!
+            maturity = datetime.fromtimestamp(int(m['maturityTime']) / 1000, timezone.utc)
+            delta = futures.days_left(maturity)
+            days = utils.days_delta_fractional(delta)
+
+            annualized = m['premium'] / days * 365
+
+            crypto = m['pair'][:3]
+            color = crypto_colors[crypto]
+            utils.cprint(f"{m['pair']} {m['product_id']} {m['tag']} {m['markPrice']} {m['premium']}% in {int(days)}d {round(annualized,1)}%", color)
+
+    # CONDITIONS FOR SENDING EMAIL
+    # send_email()
+    # send_sms()
 
 def on_error(ws, error):
     utils.cprint(error, Fore.RED)
 
 def ws_open(ws):
+    global futures
     futures = Futures()
     all_symbols = futures.all_futures_symbols()
 
@@ -49,7 +84,8 @@ def ws_open(ws):
     # subscribe = '{"event": "subscribe", "feed": "ticker", "product_ids": ["PI_XBTUSD","PI_ETHUSD"]}'
     subscribe = '{"event": "subscribe", "feed": "ticker", "product_ids": %s}' % product_ids
 
-    utils.cprint(subscribe, Fore.CYAN)
+    utils.cprint(f'subscribe {subscribe}', Fore.MAGENTA)
+
     ws.send(subscribe)
 
 def on_close(ws):
@@ -67,7 +103,7 @@ try:
     # Main Thread
     while True:
         time.sleep(5)
-        utils.cprint("Main Thread: %s" % time.ctime(time.time()), Fore.CYAN)
+        utils.cprint("Main Thread: %s" % time.ctime(time.time()), Fore.MAGENTA)
 
 except KeyboardInterrupt:
     ws.close()
